f8e014f94dc7e82d721d86ef1289fdbe71d071a8
timvoaus <146335301+timvoaus@users.noreply.github.com>
2026-03-25 17:52:29 +1100

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -4,9 +4,6 @@ const pageConfig: PageConfig = {
   // Title for your status page
   title: "NextDNS Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
-  links: [
-    // { link: 'mailto:timvo@90s.one', label: 'Email Me', highlight: true },
-  ],
   group: {
     '🌐 NextDNS': ['45_90_28_0', '45_90_30_0', 'viettel_han', 'viettel_sgn', 'lightnode_han', 'lightnode_sgn'],
     '🌐 Others': ['opendns', 'quad9', 'cloudfare', 'adguard'],
